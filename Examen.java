93bebdc33782e8b1ffcab1e752a3adbe3f8cb2b6
Alberton2233 <102196859+Alberton2233@users.noreply.github.com>
2022-05-17 20:02:11 -0600

    Update Examen.java

--- Examen.java
+++ Examen.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Examen {
     public static void main(String args[]){
        String nombre;
-       int dia, mes, año, años, dia1=16, mes1=5, año1=2022, total, totalpaga, descuento;
+       float dia, mes, año, años, dia1=16, mes1=5, año1=2022, total, descuento=0;
        Scanner captura = new Scanner(System.in);
         System.out.println("Bienvenido a la aplicacion de CompuAserrí");
         System.out.println("Digite su nombre.");
@@ -27,26 +27,30 @@ public class Examen {
         if(años<25){
             if(años>15) {
                 System.out.println("Usted aplica para un descuento Junior del 10%:");
-                descuento=10;
+                descuento=descuento+10;
             }
         }
         
         else if(años<25){
             if(años>35) {
                 System.out.println("Usted aplica para un descuento Executivo del 15%:");
-                descuento=15;
+                descuento=descuento+15;
             }
         }
         
         else //if(años>35)
         {
             System.out.println("Usted aplica para un descuento Senior del 20%");
-            descuento=20;
+            
+            descuento=descuento+20;
             
         }
         System.out.println("Digite la suma del total a pagar.");
         total = captura.nextInt();
-        totalpaga=100/descuento*total;
+        System.out.println("descuento"+descuento);
+         
+        float totalpaga=(descuento/100)*total;
+        System.out.println(totalpaga);
         System.out.println("El cliente "+nombre+" debe de pagar "+ totalpaga);
         
         
